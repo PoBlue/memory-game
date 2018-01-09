e586a9c9fda514d24216658c56dc4b38e4b04e24
PoBlue <250299430@qq.com>
2018-01-09 17:56:21 +0800

    [responcive] add media query in css

--- js/index.js
+++ js/index.js
@@ -1,43 +1,43 @@
 let symbols = ['bicycle', 'bicycle', 'leaf', 'leaf', 'cube', 'cube', 'anchor', 'anchor', 'paper-plane-o', 'paper-plane-o', 'bolt', 'bolt', 'bomb', 'bomb', 'diamond', 'diamond'],
-		opened = [],
-		match = 0,
-		moves = 0,
-		$deck = $('.deck'),
-		$scorePanel = $('#score-panel'),
-		$moveNum = $('.moves'),
-		$ratingStars = $('.fa-star'),
-		$restart = $('.restart'),
-		delay = 400,
-		currentTimer,
-    second = 0,
-    $timer = $('.timer'),
-		totalCard = symbols.length / 2,
-		rank3stars = 10,
-		rank2stars = 16,
-		rank1stars = 20;
+	opened = [],
+	match = 0,
+	moves = 0,
+	$deck = $('.deck'),
+	$scorePanel = $('#score-panel'),
+	$moveNum = $('.moves'),
+	$ratingStars = $('.fa-star'),
+	$restart = $('.restart'),
+	delay = 400,
+	currentTimer,
+	second = 0,
+	$timer = $('.timer'),
+	totalCard = symbols.length / 2,
+	rank3stars = 10,
+	rank2stars = 16,
+	rank1stars = 20;
 
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-	
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+	var currentIndex = array.length, temporaryValue, randomIndex;
 
-  return array;
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
 }
 
 // Initial Game
 function initGame() {
-  var cards = shuffle(symbols);
-  $deck.empty();
-  match = 0;
-  moves = 0;
-  $moveNum.text('0');
-  $ratingStars.removeClass('fa-star-o').addClass('fa-star');
+	var cards = shuffle(symbols);
+	$deck.empty();
+	match = 0;
+	moves = 0;
+	$moveNum.text('0');
+	$ratingStars.removeClass('fa-star-o').addClass('fa-star');
 	for (var i = 0; i < cards.length; i++) {
 		$deck.append($('<li class="card"><i class="fa fa-' + cards[i] + '"></i></li>'))
 	}
@@ -61,7 +61,7 @@ function setRating(moves) {
 	} else if (moves > rank1stars) {
 		$ratingStars.eq(0).removeClass('fa-star').addClass('fa-star-o');
 		rating = 0;
-	}	
+	}
 	return { score: rating };
 };
 
@@ -75,7 +75,7 @@ function endGame(moves, score) {
 		type: 'success',
 		confirmButtonColor: '#02ccba',
 		confirmButtonText: 'Play again!'
-	}).then(function(isConfirm) {
+	}).then(function (isConfirm) {
 		if (isConfirm) {
 			initGame();
 		}
@@ -83,22 +83,22 @@ function endGame(moves, score) {
 }
 
 // Restart Game
-$restart.bind('click', function() {
-  swal({
-    allowEscapeKey: false,
-    allowOutsideClick: false,
-    title: 'Are you sure?',
-    text: "Your progress will be Lost!",
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#02ccba',
-    cancelButtonColor: '#f95c3c',
-    confirmButtonText: 'Yes, Restart Game!'
-  }).then(function(isConfirm) {
-    if (isConfirm) {
-      initGame();
-    }
-  })
+$restart.bind('click', function () {
+	swal({
+		allowEscapeKey: false,
+		allowOutsideClick: false,
+		title: 'Are you sure?',
+		text: "Your progress will be Lost!",
+		type: 'warning',
+		showCancelButton: true,
+		confirmButtonColor: '#02ccba',
+		cancelButtonColor: '#f95c3c',
+		confirmButtonText: 'Yes, Restart Game!'
+	}).then(function (isConfirm) {
+		if (isConfirm) {
+			initGame();
+		}
+	})
 });
 
 var addCardListener = function () {
@@ -149,14 +149,14 @@ var addCardListener = function () {
 
 
 function initTime() {
-	currentTimer = setInterval(function() {
-			$timer.text(`${second}`)
-			second = second + 1
+	currentTimer = setInterval(function () {
+		$timer.text(`${second}`)
+		second = second + 1
 	}, 1000);
 }
 
 function resetTimer(timer) {
-	if(timer) {
+	if (timer) {
 		clearInterval(timer);
 	}
 }
